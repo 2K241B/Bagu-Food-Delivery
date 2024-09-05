9e999875a39dfd1d84860d44c65bee2a48401552
bagunaa <bagunaa.m@gmail.com>
2024-09-05 15:33:25 +0800

    delete user

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -56,3 +56,13 @@ export const getUpdateUser = async (req, res) => {
     res.status(500).send(error.message);
   }
 };
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userModel.findByIdAndDelete(id);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+};
