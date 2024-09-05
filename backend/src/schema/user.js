bf70f06dbf4e1e691f0d91846528aec226be12ea
bagunaa <bagunaa.m@gmail.com>
2024-09-05 17:14:58 +0800

    be-schema-validation

--- backend/src/schema/user.js
+++ backend/src/schema/user.js
@@ -1,10 +1,24 @@
 import { Schema, model } from "mongoose";
 
 const UserSchema = {
-  name: String,
-  email: String,
-  password: String,
-  phonenumber: String,
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    minlength: [12, "Please enter a valid email"],
+    maxlength: [50, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  phonenumber: {
+    type: String,
+    required: [true, "Password is required"],
+  },
   role: {
     type: String,
     enum: ["user", "admin"],
