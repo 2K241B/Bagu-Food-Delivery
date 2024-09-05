cb43319780c374288dda0da43c354ec4923e6f1d
bagunaa <bagunaa.m@gmail.com>
2024-09-05 15:05:20 +0800

    update user

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createUser, getUserById, getUsers } from "../controller/user.js";
+import {
+  createUser,
+  getUpdateUser,
+  getUserById,
+  getUsers,
+} from "../controller/user.js";
 
 const user = Router();
 
 user
   .post("/create", createUser)
   .get("/getUsers", getUsers)
-  .get("/:id", getUserById);
-
+  .get("/:id", getUserById)
+  .put("/:id", getUpdateUser);
 export { user };
