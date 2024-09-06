c54d1c4e8e89346be94c3c1e86c4490b21d51fe6
bagunaa <bagunaa.m@gmail.com>
2024-09-06 18:03:42 +0800

    login dutuu

--- backend/src/controller/auth.js
+++ backend/src/controller/auth.js
@@ -1,10 +1,30 @@
 import { userModel } from "../schema/user";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export const login = async (req, res) => {
   try {
-    const response = await userModel.findOne({ email: req.body.email });
+    const user = await userModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).send("Password or email doesn't match");
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token, user: { id: user._id, email: user.email } });
   } catch (error) {
     console.error(error);
-    res.status(500).send(error.message);
+    res.status(500).send("Server error");
   }
 };
